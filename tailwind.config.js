7477613951dffd9c952640a3d8c9ba32e9c9c0bb
Olúmáyòwá Akinkuehinmi <akintunero101@gmail.com>
2024-12-20 00:59:29 +0100

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+// Project setup - Sat Jun 21 02:05:48 WAT 2025
